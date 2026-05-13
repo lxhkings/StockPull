bba70abef313beb16bcc94e5866a0d4a29ba80a6
lxhkings <lxhkings@gmail.com>
2026-05-13 20:12:38 +0800

    chore: 删除硬编码密码脚本 clear_sp500_sync.py

--- scripts/clear_sp500_sync.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-conn = pymysql.connect(host='192.168.8.9', port=3306, user='root', password='18620001807@Aa', database='stocks')
-cur = conn.cursor()
-cur.execute("DELETE FROM sync_log WHERE ticker IN (SELECT ticker FROM index_constituents WHERE index_id='SP500')")
-conn.commit()
-print(f'Deleted {cur.rowcount} sync_log records')
-conn.close()
